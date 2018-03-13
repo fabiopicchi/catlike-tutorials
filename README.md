ef785435713ee509c221cd7bede2045c3fd576f4
FABIO LUIZ PICCHI <fabio.picchi@bpnote-fabio>
2018-03-13 10:36:30 -0300

    fractal tutorial added

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # catlike-tutorials
 Catlike coding Unity tutorials
 http://catlikecoding.com/unity/tutorials/
+
+Garbage collection tips:
+- Don't use Invoke or StartCoroutine with a string.
+- Don't use GUILayout and flag your GUI MonoBehaviour to prevent the 800bytes/frames of GUI garbage from occurring. (http://docs.unity3d.com/ScriptReference/MonoBehaviour-useGUILayout.html)
+- Don't use GameObject.Tag or GameObject.Name
+- Never use GetComponent on Update, and cache it if possible
+- Don't use foreach
---
+++ catilike-tutorials/4/Assets/Fractal.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh[] meshes;
+	private Material[,] materials;
+	public Material material;
+	public int maxDepth;
+	private int depth;
+	public float childScale;
+	public float spawnProbability;
+	public float maxRotationSpeed;
+	private float rotationSpeed;
+
+	private Vector3[] directions = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left,
+		Vector3.forward,
+		Vector3.back
+	};
+
+	private Quaternion[] rotations = {
+		Quaternion.identity,
+		Quaternion.Euler(0f, 0f, -90f),
+		Quaternion.Euler(0f, 0f, 90f),
+		Quaternion.Euler(90f, 0f, 0f),
+		Quaternion.Euler(-90f, 0f, 0f)
+	};
+
+	void InitializeMaterials() {
+		materials = new Material[maxDepth + 1, 2];
+		for (int i = 0; i < maxDepth; i++) {
+			float t = i / (maxDepth - 1f);
+			t *= t;
+			materials[i, 0] = new Material(material);
+			materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+
+			materials[i, 1] = new Material(material);
+			materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+		}
+
+		materials[maxDepth, 0] = new Material(material);
+		materials[maxDepth, 0].color = Color.magenta;
+
+		materials[maxDepth, 1] = new Material(material);
+		materials[maxDepth, 1].color = Color.red;
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (materials == null) {
+			InitializeMaterials();
+		}
+		rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+		gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
+		MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+		meshRenderer.material = materials[depth, Random.Range(0, 2)];
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+
+	private IEnumerator CreateChildren() {
+		for (int i = 0; i < directions.Length; i++) {
+			if (Random.value < spawnProbability) {
+				yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+				new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, directions[i], rotations[i]);
+			}
+		}
+		if (depth == 0 && Random.value < spawnProbability) {
+			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.down, Quaternion.Euler(0f, 0f, 180f));
+		}
+	}
+
+	void Initialize (Fractal parent, Vector3 direction, Quaternion rotation) {
+		spawnProbability = parent.spawnProbability;
+		meshes = parent.meshes;
+		materials = parent.materials;
+		depth = parent.depth + 1;
+		maxDepth = parent.maxDepth;
+		maxRotationSpeed = parent.maxRotationSpeed;
+		transform.parent = parent.transform;
+		childScale = parent.childScale;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+		transform.localRotation = rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+	}
+}
